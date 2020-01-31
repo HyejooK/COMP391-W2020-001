2f80faf6746aa9cfb6f10815e712ce1c24131340
Hyejoo Kim <hyejoo.k225@gmail.com>
2020-01-31 13:40:59 -0500

    Bound player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables
+    public float speed;
+    public float minX, maxX, minY, maxY;
+
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        rBody.velocity = new Vector2(horiz, vert) * speed;
+
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, minX, maxX),
+            Mathf.Clamp(rBody.position.y, minY, maxY));
+
+    }
+}
